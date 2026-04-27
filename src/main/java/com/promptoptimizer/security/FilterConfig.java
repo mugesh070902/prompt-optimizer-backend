51d1fb377c5ffca30d347a7efb8dc3a2701adc3f
mugesh070902 <146244579+mugesh070902@users.noreply.github.com>
2026-04-27 10:33:10 +0530

    Update FilterConfig.java

--- src/main/java/com/promptoptimizer/security/FilterConfig.java
+++ src/main/java/com/promptoptimizer/security/FilterConfig.java
@@ -1,7 +1,21 @@
+package com.promptoptimizer.security;
+
+import org.springframework.context.annotation.*;
+import org.springframework.boot.web.servlet.*;
+
+@Configuration
+public class FilterConfig {
+
 @Bean
-public FilterRegistrationBean<JwtFilter> jwtFilter() {
-    FilterRegistrationBean<JwtFilter> reg = new FilterRegistrationBean<>();
-    reg.setFilter(new JwtFilter());
-    reg.addUrlPatterns("/api/*");
-    return reg;
+FilterRegistrationBean<JwtFilter> jwtFilter(){
+
+FilterRegistrationBean<JwtFilter> reg=
+new FilterRegistrationBean<>();
+
+reg.setFilter(new JwtFilter());
+
+reg.addUrlPatterns("/api/*");
+
+return reg;
+}
 }
