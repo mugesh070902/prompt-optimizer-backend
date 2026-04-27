3288f6c128e905045d84651cba4131b2f543599d
mugesh070902 <146244579+mugesh070902@users.noreply.github.com>
2026-04-27 10:30:43 +0530

    Update PromptHistoryRepository.java

--- src/main/java/com/promptoptimizer/repository/PromptHistoryRepository.java
+++ src/main/java/com/promptoptimizer/repository/PromptHistoryRepository.java
@@ -1,7 +1,12 @@
 package com.promptoptimizer.repository;
 
-import com.promptoptimizer.model.PromptHistory;
+import com.promptoptimizer.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface PromptHistoryRepository extends JpaRepository<PromptHistory, Long> {
+public interface PromptHistoryRepository
+extends JpaRepository<PromptHistory,Long>{
+
+List<PromptHistory> findByUser(User user);
+
 }
